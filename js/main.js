e62cbcd10ec3b709c4d65ac74f76bd6a2882896c
Jeferson Grueso <jeferson9011@gmail.com>
2020-02-17 18:51:46 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const lista = document.getElementById('lista');
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="profesion"></i>',
+        '<i class="profesion">Ingeniero Electrónico</i>',
+        '<i class="profesion">Desarrollador Frontend</i>',
+        '<i class="profesion">Profesor</i>',
+        '<i class="profesion">Técnico profesional en Redes</i>',
+        '<i class="profesion">Diseñador Empírico</i>'
+    ],
+    // Opciones de configuracion libreria typed.js
+    stringsElement: '#cadenas-texto',
+    typeSpeed: 30,
+    startDelay: 300,
+    backspeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+
+});
+
+
+
+
+
+
+Sortable.create(lista, {
+    animation: 350,
+    chosenClass: "seleccionado",
+    ghostClass: "fantasma",
+    dragClass: "drag",
+
+    onEnd: () => {
+        console.log("Se insertó un elemento");
+    },
+    group: "lista-personas",
+    store: {
+        // Guardar orden de la lista
+        set: (sortable) => {
+            const orden = sortable.toArray();
+
+            localStorage.setItem(sortable.options.group.name, orden.join('|'));
+            console.log(orden);
+        },
+        // Obtener orden de la lista
+        get: (sortable) => {
+            const orden = localStorage.getItem(sortable.options.group.name);
+            return orden ? orden.split('|') : [];
+        }
+
+    }
+});
